1201cc6e01f85c5bf0c946867369bac7e7f296ff
Kelvin Lukorito <kelvin.lukorito@andela.com>
2019-05-06 02:57:55 +0300

    add site content

---
+++ src/components/sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import {useStaticQuery, graphql} from 'gatsby';
+import Img from 'gatsby-image';
+
+
+export default () => {
+
+    const data = useStaticQuery(graphql`
+        query ProfileImageQuery{
+            profileImage: file(relativePath: {eq: "Kelvin_Kitika-4-X3.jpg"}){
+                childImageSharp {
+                    fixed(width: 125, height: 125) {
+                    ...GatsbyImageSharpFixed
+                    } 
+                }
+            }
+        }
+    `)
+
+    return (
+        <aside className="sidebar">
+            <section className="sidebar section">
+                <Link to='/'>
+                    <Img fixed={data.profileImage.childImageSharp.fixed} className="profile-image"/>
+                </Link>
+                <div className="name">Kelvin Kitika</div>
+                <div className="job-title">Junior Javascript Engineer</div>
+            </section>
+            <section className="sidebar section sidebar-section white">
+                <nav className="nav">
+                    <ul>
+                        <Link to='/'>About Me</Link>
+                    </ul>
+                </nav>
+            </section>
+            <section className="sidebar section sidebar-section"></section>
+        </aside>
+        
+    )
+}
+
---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from 'gatsby';
+
+const AboutPage = () => (
+  <main>
+      <section>
+          <h1>About Me</h1>
+          <p>
+              Hi, I'm Kelvin. I live in Nairobi, Kenya and I am a passionate web developer.
+              Currently, I work at Andela, Kenya as a Software Engineer specializing on implementing 
+              front end designs.
+          </p>
+
+          <h2>Contact</h2>
+          <p>If you want to get in touch with my, hit me up at <a href="mailto:me@kitika.dev">me@kitika.dev</a> or you can connect with me through:</p>
+          <ul>
+              <li><a href='http://www.github.com/lukorito'>Github</a></li>
+              <li><a href='http://www.twitter.com/_kitika'>Twitter</a></li>
+          </ul>
+      </section>
+  </main>
+)
+
+export default AboutPage
