bf37642b51ad605ebb3ec21497ce19d1a76308f7
Kelvin Lukorito <kelvin.lukorito@andela.com>
2019-05-06 03:12:59 +0300

    stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import Layout from '../components/layout';
 import SideBar from '../components/sidebar';
 import AboutPage from '../pages/about';
-import SEO from '../components/SEO'
-import {Helmet} from "react-helmet";
+import SEO from '../components/SEO';
 
 const IndexPage = () => (
   <Layout>
